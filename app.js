4d3a30fb82ba22c1f0da63f2849f863c96fba754
vishu3278 <vishu3278@gmail.com>
2016-02-23 12:01:21 +0530

    m

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function checkConnection() {
     states[Connection.CELL]     = 'Cell generic connection';
     states[Connection.NONE]     = 'No network connection';
 
-    alert('Connection type: ' + states[networkState]);
+//    alert('Connection type: ' + states[networkState]);
+    return(states[networkState]);
 }
 
-//checkConnection();
